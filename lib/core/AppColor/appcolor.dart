328fd9b34e737b3aca9513aed5d6d8a4c65a63af
Salah Saad <kassabksab@gmail.com>
2024-08-05 20:29:16 +0300

    add App Color

---
+++ lib/core/AppColor/appcolor.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static final Color mainColor1 = Color(0xff00629B);
+  static final Color Textcolor = Color(0xff001355);
+  static final Color SecColor = Color(0xff002855);
+  static final Color Color1 = Color(0xffF1F1F1);
+  static final Color Color2 = Color(0xffffffff);
+}
