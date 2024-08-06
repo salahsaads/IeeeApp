72553741f3f04da85bd6ae2bf3d34bebe731f830
Shahd-Elwahdan <shahdelwahdan@gmail.com>
2024-08-07 00:48:46 +0300

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:device_preview/device_preview.dart';
 import 'package:ieee/Features/Nav/presntation/view/Nav.dart';
+import 'package:ieee/Features/splash/splashscreen.dart';
 
 void main() => runApp(
       DevicePreview(
@@ -17,6 +18,6 @@ class IEEE extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Nav());
+    return MaterialApp(home: SplashScreen());
   }
 }
